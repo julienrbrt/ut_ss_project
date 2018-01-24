9035d29a511ff0fdf068bc6d20997da69a19cd7d
R. Santing <richardonar@gmail.com>
2018-01-24 11:32:56 +0100

    Tools compiler issues fixed

--- src/tools/Tools.java
+++ src/tools/Tools.java
@@ -8,14 +8,8 @@
 package tools;
 
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-
-import javax.swing.*;
 
 public class Tools {
 	
