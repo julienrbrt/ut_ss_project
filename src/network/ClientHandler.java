2adb1f16f548f0e7801f050828cfb0ad21aad414
Julien <julien@rbrt.fr>
2018-01-25 00:18:08 +0100

    using last year server has base even if wasn't working

--- src/network/ClientHandler.java
+++ src/network/ClientHandler.java
@@ -1,5 +1,75 @@
 package network;
 
-public class ClientHandler {
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
+import game.*;
+import game.player.*;
+
+/**
+ * This class for defining all the method used in the Client of the game.
+ * @author Yordi Hazekamp 
+ * @author Julien Robert
+ * @version 0.1
+ */
+
+// MVC (Controller)
+public class ClientHandler implements Protocol  {
+	
+	public static List<String> players;
+	Board board = new game.Board();
+
+    /**
+     * Connected clients use it to enter the lobby.
+     * @return Join and the user username
+     */
+    public String join(String username) {
+    	players.add(username);
+    	return "Join " + username;
+    }
+    
+    /**
+     * Clients in the lobby use it to start a game. When the required number of
+	players are “ready”, the game will begin.
+	* @return Ready and the number of players
+     */
+    public String ready() {
+    	if (players.size() % 2 != 0) {
+    		return "Ready 2";
+    	} else {
+    		return "You have to be two to play the game";
+    	}
+    }
+    
+    /**
+     * A client leaves the game. When this happens, all the players go to the lobby.
+     * Not implemented, leaving quit the client
+     */
+    public String leave() {
+    	disconnect();
+    	return "Leave";
+    }
+    
+    /**
+     * A client can use this command to disconnect from the server.
+     */
+    public void disconnect() {
+    }
+    
+    /**
+     * The player notifies the server of the move he wishes to perform.
+     * @return the move done by the user
+     */
+    public String move(int x, int z) {
+    	return "Move " + x + " " + z;
+    }    
+    
+    /**
+     * @return list of players
+     */
+    public List<String> getPlayer() {
+    	return players;
+    }
+    
 }
--- src/network/ServerHandler.java
+++ src/network/ServerHandler.java
@@ -1,5 +1,68 @@
 package network;
 
-public class ServerHandler {
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
+import game.*;
+import game.player.Mark;
+
+/**
+ * This class for defining all the method used in the Server of the game.
+ * @author Yordi Hazekamp 
+ * @author Julien Robert
+ * @version 0.1
+ */
+
+public class ServerHandler implements Protocol {
+	
+    public ClientHandler client;
+	List<String> players = client.getPlayer();
+	Board board;
+	
+	/**
+	* The server notifies the clients of the move that was performed.
+	*/
+	public String notifyMove(int x, int z) {
+        if (x >= Board.DIM || z >= Board.DIM) {
+        	return error();
+        }
+		boolean valid = board.isField(x, z, board.getY(x, z))
+        		&& board.isEmpty(x, z, board.getY(x, z));
+		if (valid) {
+			return "NotifyMove " + x + " " + z;
+		} else {
+			return error();
+		}
+		
+	}
+	    
+	/**
+	* The server notifies the clients that the game has started.
+	*/
+	public String startGame() {
+		return players.get(0) + " " + players.get(1);
+	}
+	    
+	/**
+	* The server notifies the clients that the game is over.
+	*/
+	public String gameOver() {
+	    if (board.gameOver()) {
+	    	if (board.isWinner(Mark.BB)) {
+	    		return "Winner" + players.get(0);
+	    	} else {
+	    		return "Winner" + players.get(1);
+	    	}
+	    }
+	    return error();
+	}
+	 
+	/**
+	* The server notifies the client of an error that occurred.
+	*/
+	public String error() {
+		return "Something wrong happened";
+	}
+	
 }
