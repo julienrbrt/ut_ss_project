51be16b551c4167f9bd151f23e2f96934285a2a9
Julien <julien@rbrt.fr>
2018-01-26 01:56:01 +0100

    Starting with server

--- src/gui/ServerSession.java
+++ src/gui/ServerSession.java
@@ -1,11 +1,81 @@
 package gui;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import tools.Tools;
 import javax.swing.JOptionPane;
 
-public class ServerSession {
+import network.Peer;
+
+/**
+ * This class for maintening the Server of the Ringgz game for playing in network.
+ * @author Richard
+ * @author Julien
+ * @version 0.1
+ */
+public class ServerSession extends Observable {
+	
+	String name;
+	int port;
 	
 	public ServerSession() {
-		JOptionPane.showMessageDialog(null, "Server only available with command line.");
+		init();
+        ServerSocket ssock = null;
+        Socket sock = null;
+        
+        /**
+         * try to open a Socket to the server
+         */
+        try {
+            ssock = new ServerSocket(port);
+        } catch (IOException e) {
+        	JOptionPane.showMessageDialog(null,
+        			"ERROR: could not create a server socket on port " + port);
+        }
+        
+        try {
+        	int i = 0;
+        	while (sock == null) {
+        		sock = ssock.accept();
+//        		setChanged();
+//        		notifyObservers("new");
+        		System.out.println("Client connected: " + (++i));              
+        	}
+        } catch (IOException e) {
+        	JOptionPane.showMessageDialog(null,
+        			"ERROR: could not create socket.");
+        }
+        
+        /**
+         *  create Peer object and start the two-way communication
+         */
+        try {
+            Peer server = new Peer(name, sock);
+            Thread streamInputHandler = new Thread(server);
+            streamInputHandler.start();
+            server.serverside();
+            server.shutDown();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public void init() {
+
+        boolean valid = false;
+	    
+	    name = JOptionPane.showInputDialog(null, "Set Server name");
+        String askPort = JOptionPane.showInputDialog(null, "Set listening port");
+	       
+		while (!valid) {
+			if (!Tools.validNum(askPort)) {
+				JOptionPane.showMessageDialog(null, "Port invalid.");
+				askPort = JOptionPane.showInputDialog(null, "Set listening port");
+			}
+		}
+		
+		port = Integer.parseInt(askPort);
 	}
 	
 }
--- src/tools/Tools.java
+++ src/tools/Tools.java
@@ -12,33 +12,7 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 public class Tools {
-	
-	public static boolean validIP(String ip) {
-	    try {
-	        if (ip == null || ip.isEmpty()) {
-	            return false;
-	        }
-
-	        String[] parts = ip.split("\\.");
-	        if (parts.length != 4) {
-	            return false;
-	        }
-
-	        for (String s : parts) {
-	            int i = Integer.parseInt(s);
-	            if ((i < 0) || (i > 255)) {
-	                return false;
-	            }
-	        }
-	        if (ip.endsWith(".")) {
-	            return false;
-	        }
-	        return true;
-	    } catch (NumberFormatException nfe) {
-	        return false;
-	    }
-	}
-	
+		
 	public static boolean validNum(String number) {
 		try {
 			Integer.parseInt(number);
