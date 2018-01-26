b7d2fc38aafd1dbe5c26211ffd863538acd3d8f8
Julien <julien@rbrt.fr>
2018-01-26 22:22:07 +0100

    Command defined.

--- src/network/ClientHandler.java
+++ src/network/ClientHandler.java
@@ -1,73 +1,36 @@
 package network;
 
-import java.io.*;
-import java.net.*;
 import java.util.*;
-
-import game.*;
-import game.player.*;
 
 /**
  * This class for defining all the method used in the Client of the game.
+ * For comment see Protocol.java
  * @author Richard
  * @author Julien
  * @version 0.1
  */
 
-// MVC (Controller)
 public class ClientHandler implements Protocol  {
 	
 	public static List<String> players;
-	Board board = new game.Board();
 
-    /**
-     * Connected clients use it to enter the lobby.
-     * @return Join and the user username
-     */
-    public String join(String username) {
+    public String joinrequest(String username) {
     	players.add(username);
-    	return "Join " + username;
+    	return CLIENT_JOINREQUEST + " " + username + " 0 0 0 0";
     }
     
-    /**
-     * Clients in the lobby use it to start a game. When the required number of
-	players are “ready”, the game will begin.
-	* @return Ready and the number of players
-     */
-    public String ready() {
-    	if (players.size() % 2 != 0) {
-    		return "Ready 2";
-    	} else {
-    		return "You have to be two to play the game";
-    	}
+    public String gamerequest(int playersNum) {
+    	return CLIENT_GAMEREQUEST + " " + playersNum;
     }
     
-    /**
-     * A client leaves the game. When this happens, all the players go to the lobby.
-     * Not implemented, leaving quit the client
-     */
-    public String leave() {
-    	disconnect();
-    	return "Leave";
+    public String setmove(int x, int y, int size, int color) {
+    	return CLIENT_SETMOVE + " " + x + " " + y + " " + size + " " + color;
     }
     
-    /**
-     * A client can use this command to disconnect from the server.
-     */
-    public void disconnect() {
+    public String disconnect() {
+    	return "disconnect";
     }
-    
-    /**
-     * The player notifies the server of the move he wishes to perform.
-     * @return the move done by the user
-     */
-    public String move(int x, int z) {
-    	return "Move " + x + " " + z;
-    }    
-    
-    /**
-     * @return list of players
-     */
+      
     public List<String> getPlayer() {
     	return players;
     }
--- src/network/ServerHandler.java
+++ src/network/ServerHandler.java
@@ -1,14 +1,12 @@
 package network;
 
-import java.io.*;
-import java.net.*;
 import java.util.*;
 
 import game.*;
-import game.player.Color;
 
 /**
  * This class for defining all the method used in the Server of the game.
+ * For comment see Protocol.java
  * @author Richard
  * @author Julien
  * @version 0.1
@@ -20,49 +18,56 @@ public class ServerHandler implements Protocol {
 	List<String> players = client.getPlayer();
 	Board board;
 	
-	/**
-	* The server notifies the clients of the move that was performed.
-	*/
-	public String notifyMove(int x, int y) {
-        if (x >= Board.SIZE || y >= Board.SIZE) {
-        	return error();
-        }
-		boolean valid = board.isField(x, y, board.getY(x, y))
-        		&& board.isEmpty(x, z, board.getY(x, y));
-		if (valid) {
-			return "NotifyMove " + x + " " + y;
-		} else {
-			return error();
-		}
+
+	public String acceptjoin() {
+		return SERVER_ACCEPTJOIN + " 0 0 0 0";
+	}
+	
+	public String denyjoin(String username) {
+		return SERVER_DENYJOIN + " " + username;
+	}
+	
+	public String startgame(String[] usernames) {
 		
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int x = 0; x < usernames.length; x++) {
+			stringBuilder.append(usernames[x] + " ");
+		}
+	
+		return SERVER_STARTGAME + stringBuilder.toString();
 	}
-	    
-	/**
-	* The server notifies the clients that the game has started.
-	*/
-	public String startGame() {
-		return players.get(0) + " " + players.get(1);
+	
+	public String moverequest(String username) {
+		return SERVER_MOVEREQUEST + " " + username;
 	}
-	    
-	/**
-	* The server notifies the clients that the game is over.
-	*/
-	public String gameOver() {
-	    if (board.gameOver()) {
-	    	if (board.isWinner(Mark.BB)) {
-	    		return "Winner" + players.get(0);
-	    	} else {
-	    		return "Winner" + players.get(1);
-	    	}
-	    }
-	    return error();
+	
+	public String denymove(String username) {
+		return SERVER_DENYMOVE + " " + username;
 	}
-	 
-	/**
-	* The server notifies the client of an error that occurred.
-	*/
-	public String error() {
-		return "Something wrong happened";
+	
+	public String notifymove(int x, int y, int size, int color) {
+		return SERVER_NOTIFYMOVE + " " + x + " " + y + " " + size + " " + color;
+	}
+	
+	public String gameover(String[] winners) {
+		
+		// When tie
+		if (winners.length > 1) {
+			StringBuilder stringBuilder = new StringBuilder();
+			for (int x = 0; x < winners.length; x++) {
+				stringBuilder.append(winners[x] + " ");
+			}
+		}
+
+		return SERVER_GAMEOVER + " " + winners;
+	}
+	
+	public String connectionlost(String username) {
+		return SERVER_CONNECTIONLOST + " " + username;
+	}
+	
+	public String invalidcommand() {
+		return SERVER_INVALIDCOMMAND;
 	}
 	
 }
