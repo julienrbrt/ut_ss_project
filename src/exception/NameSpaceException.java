4d98ac5dfaadfcaad7bb6b82ceffcd1c896d8448
R. Santing <richardonar@gmail.com>
2018-01-29 11:53:44 +0100

    Javadoc'ed the exceptions, just in case we need to

--- src/exception/NameSpaceException.java
+++ src/exception/NameSpaceException.java
@@ -1,11 +1,21 @@
 package exception;
 
+/**
+ * Exception to be thrown when a name is typed in the inputfield that contains at least one space
+ * @author Richard
+ * @author Julien
+ */
+
 public class NameSpaceException extends Exception {
 
 	/**
 	 * Generated UID.
 	 */
 	private static final long serialVersionUID = 3461535333680139264L;
+	
+	/**
+	 * Constructor for NameSpaceException
+	 */
 	
 	public NameSpaceException() {
 		super("No spaces allowed in names.");
--- src/exception/NameUsedException.java
+++ src/exception/NameUsedException.java
@@ -1,11 +1,21 @@
 package exception;
 
+/**
+ * Exception to be thrown when a name typed in the inputfield is already in use somewhere
+ * @author Richard
+ * @author Julien
+ */
+
 public class NameUsedException extends Exception {
 
 	/**
 	 * Generated UID.
 	 */
 	private static final long serialVersionUID = -8668123252971416445L;	
+	
+	/**
+	 * Constructor for NameUsedException
+	 */
 	
 	public NameUsedException() {
 		super("Name already in used.");
