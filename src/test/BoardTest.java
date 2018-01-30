cbf09d168fd4bf8c04c12680800bbbfb8d5f46f7
Julien <julien@rbrt.fr>
2018-01-30 14:05:09 +0100

    checkstyle

--- src/test/BoardTest.java
+++ src/test/BoardTest.java
@@ -44,28 +44,38 @@ public class BoardTest {
 	
 	@Test
 	public void canPlaceTest() {
-		assertTrue(board2p.canPlaceCheck(2, 2, true, 3, Color.REDDD, 0));	//check whether or not the starting base can be placed in the middle
+		//check whether or not the starting base can be placed in the middle
+		assertTrue(board2p.canPlaceCheck(2, 2, true, 3, Color.REDDD, 0));
 		board2p.addHome(2, 2);	//actually place the base now, also tests functionality of addHome
-		assertFalse(board2p.canPlaceCheck(2, 2, false, 2, Color.REDDD, 0));	//make sure there is something in the (2,2) spot
-		assertFalse(board2p.canPlaceCheck(-1, 2, true, 2, Color.REDDD, 0));	//check invalid values
+		//make sure there is something in the (2,2) spot
+		assertFalse(board2p.canPlaceCheck(2, 2, false, 2, Color.REDDD, 0));
+		//check invalid values
+		assertFalse(board2p.canPlaceCheck(-1, 2, true, 2, Color.REDDD, 0));
 		assertFalse(board2p.canPlaceCheck(5, 2, true, 2, Color.REDDD, 0));
 		assertFalse(board2p.canPlaceCheck(2, -1, true, 2, Color.REDDD, 0));
-		assertFalse(board2p.canPlaceCheck(2,5, true, 2, Color.REDDD, 0));
+		assertFalse(board2p.canPlaceCheck(2, 5, true, 2, Color.REDDD, 0));
 		assertFalse(board2p.canPlaceCheck(2, 2, true, 2, null, 0));
 		assertFalse(board2p.canPlaceCheck(2, 2, true, -1, Color.REDDD, 0));
-		assertTrue(board2p.canPlaceCheck(2, 3, true, 1, Color.BLUEE, 1));	//test surrounding-color checking
+		//test surrounding-color checking
+		assertTrue(board2p.canPlaceCheck(2, 3, true, 1, Color.BLUEE, 1));
 		board3p.addHome(2, 2);
-		assertTrue(board3p.canPlace(2, 3, false, 0, Color.YELLO, 0));	//canPlace() does not actually place the piece, but it does reduce the ring counter
-		assertFalse(board3p.canPlace(2, 3, false, 0, Color.YELLO, 0));	//should return false, because player 0 only has 1 yellow ring of size 0 and it has already been counted down
+		//canPlace() does not actually place the piece, but it does reduce the ring counter
+		assertTrue(board3p.canPlace(2, 3, false, 0, Color.YELLO, 0));
+		//should return false, because player 0 only has 1 yellow ring of size 0
+		// and it has already been counted down
+		assertFalse(board3p.canPlace(2, 3, false, 0, Color.YELLO, 0));
 		board4p.canPlace(2, 2, true, 1, Color.REDDD, 0);	//flip start boolean to false
 		board4p.addHome(2, 2);
-		assertFalse(board4p.canPlaceCheck(3, 3, true, 1, Color.BLUEE, 1));	//not next to starting base
+		//not next to starting base
+		assertFalse(board4p.canPlaceCheck(3, 3, true, 1, Color.BLUEE, 1));
 		board4p.canPlace(2, 3, false, 0, Color.BLUEE, 1);
 		board4p.addRing(2, 3, false, 0, Color.BLUEE);
-		assertTrue(board4p.canPlaceCheck(3, 3, true, 1, Color.BLUEE, 1));	//next to just placed piece
+		//next to just placed piece
+		assertTrue(board4p.canPlaceCheck(3, 3, true, 1, Color.BLUEE, 1));
 		board4p.canPlace(3, 3, true, 1, Color.BLUEE, 1);
 		board4p.addRing(3, 3, true, 1, Color.BLUEE);
-		assertFalse(board4p.canPlaceCheck(3, 4, true, 0, Color.BLUEE, 1));	//check same-colored bases not being able to be placed next to each other
+		//check same-colored bases not being able to be placed next to each other
+		assertFalse(board4p.canPlaceCheck(3, 4, true, 0, Color.BLUEE, 1));
 	}
 
 }
